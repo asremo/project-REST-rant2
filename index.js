4682f778ca2fcbb7feab8290d6ff151e3958343f
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-08 20:34:24 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+// **Modules and Globals**
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// **Express Settings**
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public')) //static files don't dynamically change
 
+
+// **Controllers & Routes**
 app.use('/places', require('./controllers/places'))
 
-// when you call the render method, it knows to look for a 'views' folder
+// !! when you call the render method, it knows to look for a 'views' folder !!
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 
-
-// make sure this route is at the bottom OR 
-// it will override your other pages
+// !! make sure this route is at the bottom OR 
+// it will override your other pages !!
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
---
+++ views/places/new.jsx
@@ -0,0 +1,6 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_form () {}
+
+module.exports = new_form
