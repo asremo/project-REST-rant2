d2f8c1fd9747c6b78de0ea5392fd953bac3e4ff5
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-13 11:40:36 -0400

    added validations and custom errors

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -17,7 +17,8 @@ function edit_form (data) {
                                 id="name" 
                                 name="name" 
                                 value={data.place.name} 
-                                required />
+                                required 
+                            />
                         </div>
                         <div className='form-group col-sm-6'>
                             <label htmlFor="pic">Place Picture</label>
@@ -25,7 +26,8 @@ function edit_form (data) {
                                 className='form-control' 
                                 id="pic" 
                                 name="pic" 
-                                value={data.place.pic} />
+                                value={data.place.pic} 
+                            />
                         </div>
                     </div>
                     <div className='row'>
@@ -35,7 +37,8 @@ function edit_form (data) {
                                 className='form-control' 
                                 id="city" 
                                 name="city" 
-                                value={data.place.city} />
+                                value={data.place.city} 
+                            />
                         </div>
                         <div className='form-group col-sm-6'>
                             <label htmlFor="state">State</label>
@@ -43,7 +46,8 @@ function edit_form (data) {
                                 className='form-control' 
                                 id="state" 
                                 name="state" 
-                                value={data.place.state} />
+                                value={data.place.state} 
+                            />
                         </div>
                     </div>
                     <div className='form-group'>
@@ -53,12 +57,14 @@ function edit_form (data) {
                             id="cuisines" 
                             name="cuisines" 
                             value={data.place.cuisines} 
-                            required />
+                            required 
+                        />
                     </div>
                     <input 
                         className='btn btn-primary' 
                         type="submit" 
-                        value="Update Place" />
+                        value="Update Place" 
+                    />
                 </form>
             </main>
         </Def>
