5e8f5b9c34a51b51117729ee22dda3a6848bbe5a
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-13 15:51:27 -0400

    add comment functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -77,28 +77,28 @@ function show (data) {
                     <hr />
                     <h2 class="color">Got Your Own Rant or Rave?</h2>
                     <form action={`/places/${data.place.id}/comment`} method="POST">
-                    <div className="row">
-                        <div className="form-group col-sm-12">
-                            <label htmlFor="content">Content</label>
-                            <textarea id="content" name="content" className="form-control"></textarea>
+                        <div className="row">
+                            <div className="form-group col-sm-12">
+                                <label htmlFor="content">Content</label>
+                                <textarea id="content" name="content" className="form-control"></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="form-group col-sm-4">
-                            <label htmlFor="author">Author</label>
-                            <input id="author" name="author" className="form-control" />
+                        <div className="row">
+                            <div className="form-group col-sm-4">
+                                <label htmlFor="author">Author</label>
+                                <input id="author" name="author" className="form-control" />
+                            </div>
+                            <div className="form-group col-sm-4">
+                                <label htmlFor="stars">Star Rating</label>
+                                <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-range" />
+                            </div>
+                            <div className="form-group col-sm-2">
+                                <label htmlFor="rant">Rant?</label>
+                                <br />
+                                <input type="checkbox" id="rant" name="rant" className="form-check-input" />
+                            </div>
                         </div>
-                        <div className="form-group col-sm-4">
-                            <label htmlFor="stars">Star Rating</label>
-                            <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-range" />
-                        </div>
-                        <div className="form-group col-sm-2">
-                            <label htmlFor="rant">Rant?</label>
-                            <br />
-                            <input type="checkbox" id="rant" name="rant" className="form-check-input" />
-                        </div>
-                    </div>
-                    <input type="submit" className="btn btn-primary" value="Add Comment" />
+                        <input type="submit" className="btn btn-primary" value="Add Comment" />
                     </form>
                 </div>
             </main>
