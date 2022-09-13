15c8049375d4184f4ef0b4aa8180e85c189fd1d0
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-13 01:55:03 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,28 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/thai-food.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'images/cat-cafe.jpg'
-    }]
-      
     res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    // adds the new place's data, found in req.body, to the places array
+    places.push(req.body) 
+    // redirect = redirects to new route
+    res.redirect('/places')
 })
-  
+
 
 // make sure /new route is above /:id route so the 
 // new part of /places/new is not interpreted as an id
