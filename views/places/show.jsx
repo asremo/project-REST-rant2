cd3bd94bb547efb9a080abf3d82aec2dcd6f6034
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-09-13 16:05:16 -0400

    average stars in comment commit

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,7 +8,21 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length 
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className='border col-sm-4'>
@@ -41,9 +55,7 @@ function show (data) {
                         <h2 class="color">
                             Rating
                         </h2>
-                        <h3>
-                            Not rated
-                        </h3>
+                        {rating}
                         <br />
                         <h2 class="color">
                             Description
